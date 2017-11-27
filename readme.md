82ee4dad38cda6b45cca4c864322b0c2e57bfec9
Yingzhe Reginald Fu <fuyingzhe@berkeley.edu>
2017-11-27 14:49:04 -0800

    first commit

---
+++ readme.md
@@ -0,0 +1,15 @@
+D3 with Socket-IO Demo
+=============
+
+A quick and dirty D3 with Socket-IO demo. The NodeJS server emits a new data event periodically. D3 incorporates and displays the new data. It also throws away old data (first-in-first-out).
+
+Installation
+-----------
+
+    npm start
+
+Usage
+-----
+
+    http://localhost:99
+
---
+++ server.js
@@ -0,0 +1,54 @@
+var app = require('http').createServer(handler),
+    io = require('socket.io').listen(app), //require('socket.io').listen(80);
+    fs = require('fs');
+
+app.listen(9000);
+
+function handler(req, res) {
+    console.log('handler')
+    fs.readFile(__dirname + '/realTime.html',
+        function(err, data) {
+            if (err) {
+                res.writeHead(500);
+                return res.end('Error loading index.html');
+            }
+
+            res.writeHead(200);
+            res.end(data);
+        });
+}
+
+// Manage connections
+io.sockets.on('connection', function(socket) {
+    console.log('handle connection');
+
+    var periodInMilliseconds = 400;
+    var timeoutId = -1;
+
+    /**
+     * Handle "disconnect" events.
+     */
+    var handleDisconnect = function() {
+        console.log('handle disconnect');
+
+        clearTimeout(timeoutId);
+    };
+
+    /**
+     * Generate a request to be sent to the client.
+     */
+    var generateServerRequest = function() {
+        console.log('generate server request');
+
+        socket.emit('server request', {
+            date: new Date(),
+            value: Math.pow(Math.random(), 2)
+        });
+
+        timeoutId = setTimeout(generateServerRequest, periodInMilliseconds);
+    };
+
+    socket.on('disconnect', handleDisconnect);
+
+    timeoutId = setTimeout(generateServerRequest, periodInMilliseconds);
+});
